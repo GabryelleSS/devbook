5b0f5c8a7d3ae858076327aaa6553dce0320138c
GabryelleSS <gabryelle-gaby@hotmail.com>
2025-09-04 08:12:18 -0300

    feat: create routes user

---
+++ api/src/router/routes/users.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var userRoutes = []Route{
+	{
+		URI: "/users",
+		Method: http.MethodPost,
+		Function: controllers.CreateUser,
+		IsRequerAuthenticator: false,
+	},
+	{
+		URI: "/users",
+		Method: http.MethodGet,
+		Function: controllers.GetUsers,
+		IsRequerAuthenticator: false,
+	},
+	{
+		URI: "/users/{userId}",
+		Method: http.MethodGet,
+		Function: controllers.GetUser,
+		IsRequerAuthenticator: false,
+	},
+	{
+		URI: "/users/{userId}",
+		Method: http.MethodPut,
+		Function: controllers.UpdateUser,
+		IsRequerAuthenticator: false,
+	},
+	{
+		URI: "/users/{userId}",
+		Method: http.MethodDelete,
+		Function: controllers.DeleteUser,
+		IsRequerAuthenticator: false,
+	},
+}
